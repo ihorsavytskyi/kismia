15e9102eaf465e3f1755845b4cd3f5368fb0ac6d
Ihor Savytskyi <ihorsavytskyi@gmail.com>
2023-02-16 01:53:32 +0200

    update(*)

---
+++ src/components/FormField/FormField.js
@@ -0,0 +1,11 @@
+import React from "react";
+const FormField = ({ children }) => {
+    return (
+        <div className="form-field">
+            { children }
+        </div>
+    )
+}
+
+export default FormField
+
--- src/pages/Registration/Registration.js
+++ src/pages/Registration/Registration.js
@@ -1,10 +1,107 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MainContainer from "../../components/MainContainer/MainContainer";
+import Text from "../../components/Text/Text";
+import Section from "../../components/Section/Section";
+import FormField from "../../components/FormField/FormField"
+import Input from "../../components/Input/Input";
 
+const useValidation = (value, validations) => {
+    const [isEmpty, setEmpty] = useState(true)
+    const [minLengthError, setMinLengthError] = useState(false)
+    const [maxLengthError, setMaxLengthError] = useState(false)
+    const [isMatchesRegExp, setMatchesRegExpError] = useState(false)
+    const [inputValid, setInputValid] = useState(false)
+
+    useEffect(() => {
+
+        for (const validation in validations) {
+            switch(validation) {
+                case 'isEmpty':
+                    value ? setEmpty(false) : setEmpty(true)
+                    break;
+                case 'minLength':
+                    value.length < validations[validation] ? setMinLengthError(true) : setMinLengthError(false)
+                    break;
+                case 'maxLength':
+                    value.length > validations[validation] ? setMaxLengthError(true) : setMaxLengthError(false)
+                    break;
+                case 'pattern':
+                    value.match(validations[validation]) == null ? setMatchesRegExpError(true) : setMatchesRegExpError(false)
+                    break;
+            }
+        }
+
+    }, [value])
+
+    useEffect(() => {
+        if(isEmpty || minLengthError || maxLengthError || isMatchesRegExp) {
+            setInputValid(false)
+        } else {
+            setInputValid(true)
+        }
+    }, [isEmpty, minLengthError, maxLengthError, isMatchesRegExp])
+
+    return {
+        isEmpty,
+        minLengthError,
+        maxLengthError,
+        isMatchesRegExp,
+        inputValid
+    }
+}
+
+const useInput = (initialValue, validations) => {
+    const [value, setValue] = useState(initialValue)
+    const [isDirty, setDirty] = useState(false)
+    const valid = useValidation(value, validations)
+
+    const onChange = (e) => {
+        setValue(e.target.value)
+    }
+
+    const onBlur = (e) => {
+        setDirty(true)
+    }
+
+    return {
+        value,
+        onChange,
+        onBlur,
+        isDirty,
+        ...valid
+    }
+}
 const Registration = () => {
+
+    const name = useInput('', {
+        isEmpty: true,
+        minlength: 2,
+        maxLength: 80,
+        pattern: '^[a-zA-Z ]*$'
+    })
+
+    console.log(name)
+
     return (
         <MainContainer>
-            <h1>Registration</h1>
+            <h1>Создать анкету</h1>
+            <Section>
+                <Text
+                    textAlign={"center"}
+                    content={"Бистрая регистрация, чтоби перейти к общению"}/>
+            </Section>
+            <Section>
+                <form className="flex flex-col w-4/5 mx-auto mt-5">
+                    <FormField>
+                        <Input attr={{
+                          type: "text",
+                          name: "name",
+                          label: "Имя:",
+                          placeholder: "Укажите имя"
+                        }}/>
+                    </FormField>
+                </form>
+            </Section>
         </MainContainer>
     )
 }
